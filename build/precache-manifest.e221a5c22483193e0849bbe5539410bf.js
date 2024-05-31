8e9bd9c01d23ec0913574ca7324877cead53dd10
Rodrigo Barros <50782890+rodrigobarroshd@users.noreply.github.com>
2024-05-31 19:24:05 -0300

    Add files via upload

---
+++ build/precache-manifest.e221a5c22483193e0849bbe5539410bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7457835596484ded8f14d72fd38bae0e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc35bfb3bb474071bb45",
+    "url": "./static/css/main.c94d8850.chunk.css"
+  },
+  {
+    "revision": "053a62724a3499c4f2b5",
+    "url": "./static/js/2.063cf18c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.063cf18c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc35bfb3bb474071bb45",
+    "url": "./static/js/main.bbb9b6d5.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "./static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "./static/media/menu.b3f49afd.svg"
+  }
+]);
---
+++ src/components/mainpages/createProduct/CreateProduct.js
@@ -0,0 +1,189 @@
+import React, {useState, useContext, useEffect} from 'react'
+import axios from 'axios'
+import {GlobalState} from '../../../GlobalState'
+import Loading from '../utils/loading/Loading'
+import {useHistory, useParams} from 'react-router-dom'
+
+const initialState = {
+    product_id: '',
+    title: '',
+    price: 0,
+    description: '',
+    localizacao: '',
+    content: '',
+    category: '',
+    _id: ''
+}
+
+function CreateProduct() {
+    const state = useContext(GlobalState)
+    const [product, setProduct] = useState(initialState)
+    const [categories] = state.categoriesAPI.categories
+    const [loading, setLoading] = useState(false)
+
+
+    const [isAdmin] = state.userAPI.isAdmin
+    const [token] = state.token
+
+    const history = useHistory()
+    const param = useParams()
+
+    const [products] = state.productsAPI.products
+    const [onEdit, setOnEdit] = useState(false)
+    const [callback, setCallback] = state.productsAPI.callback
+
+    useEffect(() => {
+        if(param.id){
+            setOnEdit(true)
+            products.forEach(product => {
+                if(product._id === param.id) {
+                    setProduct(product)
+                   
+                }
+            })
+        }else{
+            setOnEdit(false)
+            setProduct(initialState)
+            
+        }
+    }, [param.id, products])
+
+    const handleUpload = async e =>{
+        e.preventDefault()
+        try {
+            if(!isAdmin) return alert("You're not an admin")
+            const file = e.target.files[0]
+            
+
+            let formData = new FormData()
+            formData.append('file', file)
+
+            setLoading(true)
+            const res = await axios.post('/api/upload', formData, {
+                headers: {'content-type': 'multipart/form-data', Authorization: token}
+            })
+            setLoading(false)
+           
+
+        } catch (err) {
+            alert(err.response.data.msg)
+        }
+    }
+
+    const handleDestroy = async () => {
+        try {
+            if(!isAdmin) return alert("You're not an admin")
+            setLoading(true)
+            await axios.post('/api/destroy', {public_id: product}, {
+                headers: {Authorization: token}
+            })
+            setLoading(false)
+            
+        } catch (err) {
+            alert(err.response.data.msg)
+        }
+    }
+
+    const handleChangeInput = e =>{
+        const {name, value} = e.target
+        setProduct({...product, [name]:value})
+    }
+
+    const handleSubmit = async e =>{
+        e.preventDefault()
+        try {
+            if(!isAdmin) return alert("You're not an admin")
+            
+
+            if(onEdit){
+                await axios.put(`/api/products/${product._id}`, {...product}, {
+                    headers: {Authorization: token}
+                })
+            }else{
+                await axios.post('/api/products', {...product}, {
+                    headers: {Authorization: token}
+                })
+            }
+            setCallback(!callback)
+            history.push("/")
+        } catch (err) {
+            alert(err.response.data.msg)
+        }
+    }
+
+    const styleUpload = {
+        display: product ? "block" : "none"
+    }
+    return (
+        <div className="create_product">
+            <div className="upload">
+                <input type="file" name="file" id="file_up" onChange={handleUpload}/>
+                {
+                    loading ? <div id="file_img"><Loading /></div>
+
+                    :<div id="file_img" style={styleUpload}>
+                        <img src={product ? product.content : ''} alt=""/>
+                        <img src={product ? product.localizacao : ''} alt=""/>
+                        <span onClick={handleDestroy}>X</span>
+                    </div>
+                }
+                
+            </div>
+
+            <form onSubmit={handleSubmit}>
+                <div className="row">
+                    <label htmlFor="product_id">Product ID</label>
+                    <input type="text" name="product_id" id="product_id" required
+                    value={product.product_id} onChange={handleChangeInput} disabled={onEdit} />
+                </div>
+
+                <div className="row">
+                    <label htmlFor="title">Nome</label>
+                    <input type="text" name="title" id="title" required
+                    value={product.title} onChange={handleChangeInput} />
+                </div>
+
+                <div className="row">
+                    <label htmlFor="price">Preço</label>
+                    <input type="number" name="price" id="price" required
+                    value={product.price} onChange={handleChangeInput} />
+                </div>
+
+                <div className="row">
+                    <label htmlFor="description">Descrição</label>
+                    <textarea type="text" name="description" id="description" required
+                    value={product.description} rows="5" onChange={handleChangeInput} />
+                </div>
+
+                <div className="row">
+                    <label htmlFor="content">Url Image</label>
+                    <textarea type="text" name="content" id="content" required
+                    value={product.content} rows="7" onChange={handleChangeInput} />
+                </div>
+                <div className="row">
+                    <label htmlFor="localizacao">Localizacao</label>
+                    <textarea type="text" name="localizacao" id="localizacao" required
+                    value={product.localizacao} rows="7" onChange={handleChangeInput} />
+                </div>
+
+                <div className="row">
+                    <label htmlFor="categories">Categorias: </label>
+                    <select name="category" value={product.category} onChange={handleChangeInput} >
+                        <option value="">Selecione uma Categoria</option>
+                        {
+                            categories.map(category => (
+                                <option value={category._id} key={category._id}>
+                                    {category.name}
+                                </option>
+                            ))
+                        }
+                    </select>
+                </div>
+
+                <button type="submit">{onEdit? "Update" : "Create"}</button>
+            </form>
+        </div>
+    )
+}
+
+export default CreateProduct
